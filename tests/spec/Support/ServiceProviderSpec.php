00f1fd1698358163dd89400d741bebcec4eda720
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2014-11-18 21:58:05 +0100

    Tested functionality that was previously inside anonymous method

--- tests/spec/Support/ServiceProviderSpec.php
+++ tests/spec/Support/ServiceProviderSpec.php
@@ -2,7 +2,10 @@
 
 namespace spec\Hipchat\Support;
 
+use Guzzle\Http\Client;
+use Hipchat\Notifier;
 use Illuminate\Container\Container as Application;
+use Illuminate\Contracts\Config\Repository;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -23,5 +26,26 @@ class ServiceProviderSpec extends ObjectBehavior
         $app->bind('Hipchat\NotifierInterface', Argument::type('\Closure'))->shouldBeCalled();
         $app->alias('Hipchat\NotifierInterface', 'hipchat')->shouldBeCalled();
         $this->register();
+    }
+
+    function it_should_return_a_configured_notifier_object(
+        Application $app,
+        Repository $config,
+        Client $client,
+        Notifier $notifier
+    ) {
+        // Setup
+        $rooms = ['default' => 'configuration data'];
+        $options = ['options' => 'data'];
+
+        // Make predictions
+        $app->make('config')->shouldBeCalledTimes(2)->willReturn($config);
+        $config->get('hipchat::config.rooms')->shouldBeCalled()->willReturn($rooms);
+        $config->get('hipchat::config')->shouldBeCalled()->willReturn($options);
+        $app->make('Guzzle\Http\Client')->shouldBeCalled()->willReturn($client);
+        $app->make('Hipchat\Notifier', [$client, $rooms, $options])->shouldBeCalled()->willReturn($notifier);
+
+        // Test hypotheses.
+        $this->configureNotifier()->shouldReturn($notifier);
     }
 }
